9bec5d8a0a53fe6cbe1389062b0858f6df622065
Lakshmi Strom <lakshmialeman@gmail.com>
2019-01-16 18:09:32 -0800

    finishing updating the clean build after instructors approval.

--- client/App.js
+++ client/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { Platform, StatusBar, StyleSheet, View, Text } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
 import AppNavigator from './navigation/AppNavigator';
 
 export default class App extends React.Component {
   state = {
     isLoadingComplete: false,
+    guestLogin: true
   };
 
   render() {
     if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
       return (
+        //TODO: 
         <AppLoading
           startAsync={this._loadResourcesAsync}
           onError={this._handleLoadingError}
@@ -21,7 +23,7 @@ export default class App extends React.Component {
       return (
         <View style={styles.container}>
           {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <AppNavigator />
+        <AppNavigator />
         </View>
       );
     }
