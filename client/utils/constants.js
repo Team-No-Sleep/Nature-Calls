90486775a46e58220f4c00a25a911967e2ba1749
kpaje <kelvinmembership@gmail.com>
2019-02-07 05:59:01 -0800

    changed to just timer numbers and aligned it in level-1, added main screen gif. Piskel lied about setting it to never loop

--- client/utils/constants.js
+++ client/utils/constants.js
@@ -11,7 +11,8 @@ module.exports = {
 			//return "http://192.168.86.106:3001"
 			// return "http://192.168.0.43:3001"
 			//return "http://localhost:3001";
-			return "http://192.168.0.43:3001"
+			// return "http://192.168.0.43:3001"
+			return "http://192.168.1.232:3001"
 		}
 		if(env === "default") {
 			return Constants.manifest.extra.serverUrl;
